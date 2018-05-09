98e8a59b164036f59a5e99a494228e001948d6f3
HowoongJun <prestoxic@gmail.com>
2018-05-09 21:56:56 +0900

    180509

--- src/RevNonholonomicTrain.py
+++ src/RevNonholonomicTrain.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import rospy
-# from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist
 from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import GetModelState
+from geometry_msgs.msg import Pose
 import numpy as np
 import sys, random
 import matplotlib.pyplot as plt
@@ -14,13 +16,14 @@ import datetime
 
 # Environment Setting
 obsNumber = 1
-state_size = obsNumber * 2
-action_size = 9
-num_episodes = 181
+state_size = obsNumber * 3
+action_size = 3
+num_episodes = 1801
 boundaryRadius = 0.85
 obstacleRadius = 0.2
 agentRadius = 0.17
-movingUnit = 0.017
+linearUnit = 1
+angularUnit = 10
 
 # A2C(Advantage Actor-Critic) agent
 class A2CAgent:
@@ -41,8 +44,8 @@ class A2CAgent:
         self.actor = self.build_actor()
         self.critic = self.build_critic()
         if self.load_model:
-            self.actor.load_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Actor_Rev.h5")
-            self.critic.load_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Critic_Rev.h5")
+            self.actor.load_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Actor_Nonholonomic_Rev.h5")
+            self.critic.load_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Critic_Nonholonomic_Rev.h5")
         self.actor.get_weights()
     # approximate policy and value using Neural Network
     # actor: state is input and probability of each action is output of model
@@ -87,76 +90,77 @@ class A2CAgent:
         self.actor.fit(state, advantages, epochs=1, verbose=0)
         self.critic.fit(state, target, epochs=1, verbose=0)
 
-def stateGenerator(obsPosition, agtPosition):
+def stateGenerator(obsPosition, agtPosition, orientation):
     returnSum = []
     returnSum = returnSum + [agtPosition[0] - obsPosition[0], agtPosition[1] - obsPosition[1]]
+    returnSum = returnSum + [orientation]
     returnSum = np.reshape(returnSum, [1, state_size])
     return returnSum
 
 def takeAction(action):
-    xAction = 0
-    yAction = 0
+    # linearX = 0
+    linearX = 1
+    angularZ = 0
     if action == 0:
-        xAction = movingUnit
+        angularZ = 0
     elif action == 1:
-        xAction = movingUnit
-        yAction = movingUnit
+        angularZ = angularUnit
     elif action == 2:
-        xAction = movingUnit
-        yAction = -movingUnit       
-    elif action == 3:
-        xAction = -movingUnit
-        yAction = movingUnit
-    elif action == 4:
-        xAction = -movingUnit
-    elif action == 5:
-        xAction = -movingUnit
-        yAction = -movingUnit
-    elif action == 6:
-        yAction = -movingUnit
-    elif action == 7:
-        yAction = movingUnit
-    elif action == 8:
-        xAction = 0
-        yAction = 0
+        angularZ = -angularUnit
+    # if action == 0:
+    #     linearX = 0
+    #     angularZ = 0
+    # elif action == 1:
+    #     linearX = linearUnit
+    #     angularZ = 0
+    # elif action == 2:
+    #     linearX = 0
+    #     angularZ = angularUnit
+    # elif action == 3:
+    #     linearX = linearUnit
+    #     angularZ = angularUnit
+    # elif action == 4:
+    #     linearX = 0
+    #     angularZ = -angularUnit
+    # elif action == 5:
+    #     linearX = linearUnit
+    #     angularZ = -angularUnit
         
-    return [xAction, yAction]
+    return [linearX, angularZ]
 
 def main():
     agent = A2CAgent(state_size, action_size)
 
     obsAngleIdx= 0
     circleFlag = False
-    initRandom = random.randrange(0, 360)
+    initRandom = 270#random.randrange(0, 360)
     initPosMainRobot = [0, 0]
 
-    # twistMainRobot_pub = rospy.Publisher('simple_create/cmd_vel', Twist, queue_size=10)
-    # twistObstRobot_pub = rospy.Publisher('simple_create2/cmd_vel', Twist, queue_size=10)
+    twistMainRobot_pub = rospy.Publisher('simple_create/cmd_vel', Twist, queue_size=10)
     posMainRobot_pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size = 10)
     posObstRobot_pub = rospy.Publisher('gazebo/set_model_state', ModelState, queue_size = 10)
 
-    # twistMainRobot_msg = Twist()
-    # twistObstRobot_msg = Twist()
+    twistMainRobot_msg = Twist()
     posMainRobot_msg = ModelState()
     posObstRobot_msg = ModelState()
 
-    # twistMainRobot_msg.linear.x = 0
-    # twistMainRobot_msg.linear.y = 0
-    # twistMainRobot_msg.linear.z = 0
-    # twistMainRobot_msg.angular.x = 0
-    # twistMainRobot_msg.angular.y = 0
-    # twistMainRobot_msg.angular.z = 0
-
+    twistMainRobot_msg.linear.x = 0
+    twistMainRobot_msg.linear.y = 0
+    twistMainRobot_msg.linear.z = 0
+    twistMainRobot_msg.angular.x = 0
+    twistMainRobot_msg.angular.y = 0
+    twistMainRobot_msg.angular.z = 0
+    
     posMainRobot_msg.model_name = "simple_create"
     posObstRobot_msg.model_name = "simple_create2"
     posObstRobot_msg.pose.position.x = 1
     posObstRobot_msg.pose.position.y = 0
     posObstRobot_msg.pose.position.z = 0
-    
+
     rospy.init_node('circler', anonymous=True)
     rate = rospy.Rate(1000) #hz
 
-    obsAngle = obsAngleIdx * math.pi/180
+    obsAngle = (initRandom + obsAngleIdx) * math.pi/180
     posObstRobot_msg.pose.position.x = initPosMainRobot[0] + boundaryRadius * math.cos(obsAngle)
     posObstRobot_msg.pose.position.y = initPosMainRobot[1] + boundaryRadius * math.sin(obsAngle)
 
@@ -164,30 +168,38 @@ def main():
         done = False
         score = 0
         reward = 0
+        
         posMainRobot_msg.pose.position.x = initPosMainRobot[0]
         posMainRobot_msg.pose.position.y = initPosMainRobot[1]
         posMainRobot_msg.pose.position.z = 0
         rospy.logwarn("Episode %d Starts!", e)
         rospy.logwarn(datetime.datetime.now().strftime('%H:%M:%S'))
+        posMainRobot_pub.publish(posMainRobot_msg)
         
-        state = stateGenerator([posObstRobot_msg.pose.position.x, posObstRobot_msg.pose.position.y], [posMainRobot_msg.pose.position.x, posMainRobot_msg.pose.position.y])
+        state = stateGenerator([posObstRobot_msg.pose.position.x, posObstRobot_msg.pose.position.y], [posMainRobot_msg.pose.position.x, posMainRobot_msg.pose.position.y], 0)
         while not done:
             action = agent.get_action(state)
-            xMove = 0
-            yMove = 0
-            [xMove, yMove] = takeAction(action)
+            linearX = 0
+            angularZ = 0
+            [linearX, angularZ] = takeAction(action)
 
-            posMainRobot_msg.pose.position.x += xMove
-            posMainRobot_msg.pose.position.y += yMove
+            twistMainRobot_msg.linear.x = linearX
+            twistMainRobot_msg.angular.z = angularZ
+            # twistMainRobot_msg.angular.z = 1
+            # rospy.Subscriber('gazebo/ModelStates', Pose, subCallback)
+            model_coordinates = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
+            object_coordinates = model_coordinates("simple_create", "")
+            # rospy.logwarn("%s", object_coordinates.twist.angular.z)
 
             collisionFlag = 0
-            next_state = stateGenerator([posObstRobot_msg.pose.position.x, posObstRobot_msg.pose.position.y], [posMainRobot_msg.pose.position.x, posMainRobot_msg.pose.position.y])
-
-            if(math.sqrt((posMainRobot_msg.pose.position.x - initPosMainRobot[0])**2 + (posMainRobot_msg.pose.position.y - initPosMainRobot[1])**2) >= boundaryRadius):
+            next_state = stateGenerator([posObstRobot_msg.pose.position.x, posObstRobot_msg.pose.position.y], [object_coordinates.pose.position.x, object_coordinates.pose.position.y], object_coordinates.pose.orientation.w * 180)
+            # rospy.logwarn("%f, %f, %f", next_state[0][0], next_state[0][1], next_state[0][2])
+            # rospy.logwarn("%f", object_coordinates.pose.orientation.w * 180)
+            if math.sqrt((object_coordinates.pose.position.x - initPosMainRobot[0])**2 + (object_coordinates.pose.position.y - initPosMainRobot[1])**2) >= boundaryRadius:
                 rospy.logwarn("No Collision!")
                 collisionFlag = 1
                 done = True
-            if math.sqrt((posMainRobot_msg.pose.position.x - posObstRobot_msg.pose.position.x)**2 + (posMainRobot_msg.pose.position.y - posObstRobot_msg.pose.position.y)**2) <= obstacleRadius + agentRadius:
+            if math.sqrt((object_coordinates.pose.position.x - posObstRobot_msg.pose.position.x)**2 + (object_coordinates.pose.position.y - posObstRobot_msg.pose.position.y)**2) <= obstacleRadius + agentRadius:
                 rospy.logwarn("Collision!")
                 collisionFlag = -1
                 done = True
@@ -196,7 +208,8 @@ def main():
                 reward = 0
             else:
                 if collisionFlag == 1:
-                    reward = -1000
+                    angReward = -180/math.pi * abs(math.atan2(object_coordinates.pose.position.y, object_coordinates.pose.position.x) - math.atan2(posObstRobot_msg.pose.position.y, posObstRobot_msg.pose.position.x))
+                    reward = -500 + angReward
                 elif collisionFlag == -1:
                     reward = 1000
             agent.train_model(state, action, reward, next_state, done)
@@ -205,26 +218,28 @@ def main():
             state = next_state
 
             if done:
-                if obsAngleIdx >= 360:
-                    circleFlag = True
-                    initRandom = random.randrange(0, 360)
-                elif obsAngleIdx < 0:
+                if obsAngleIdx >= 180:
                     circleFlag = False
-                    initRandom = random.randrange(0, 360)
+                    initRandom = 90 #random.randrange(0, 360)
+                elif obsAngleIdx < 0:
+                    circleFlag = True
+                    initRandom = 270 #random.randrange(0, 360)
                 if circleFlag == False:
                     obsAngleIdx += 2
                 else:
                     obsAngleIdx -= 2
-                obsAngle = (obsAngleIdx + initRandom)*math.pi/180
+                
+                # obsAngle = (obsAngleIdx + initRandom) * math.pi/180
+                obsAngle = -90 * math.pi/180
                 posObstRobot_msg.pose.position.x = initPosMainRobot[0] + boundaryRadius * math.cos(obsAngle)
                 posObstRobot_msg.pose.position.y = initPosMainRobot[1] + boundaryRadius * math.sin(obsAngle)
-            posMainRobot_pub.publish(posMainRobot_msg)
+            twistMainRobot_pub.publish(twistMainRobot_msg)
             posObstRobot_pub.publish(posObstRobot_msg)
             rate.sleep()
 
         if e % 60 == 0:
-            agent.actor.save_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Actor_Rev.h5")
-            agent.critic.save_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Critic_Rev.h5")
+            agent.actor.save_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Actor_Nonholonomic_Rev.h5")
+            agent.critic.save_weights("/home/howoongjun/catkin_ws/src/simple_create/src/DataSave/Critic_Nonholonomic_Rev.h5")
         rospy.logwarn("Reward: %d", score)
     # while not rospy.is_shutdown():
     #     posMainRobot_msg.pose.position.x += 0.01
